6f855c662d670a61684ce3e52f3cb6c6e3799317
junemv <vintydesign@gmail.com>
2024-07-24 11:14:41 -0700

    initial commit

---
+++ src/main/java/com/plantwitch/plantwitchbackend/PlantWitchBackendApplication.java
@@ -0,0 +1,14 @@
+package com.plantwitch.plantwitchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantWitchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantWitchBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/plantwitch/plantwitchbackend/PlantWitchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantwitch.plantwitchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantWitchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
