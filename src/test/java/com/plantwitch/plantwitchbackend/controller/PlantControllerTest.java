4916266912b49037b6e1f0d81e6e4262411aa041
Natalya <chshebetun@gmail.com>
2024-07-28 09:49:02 -0700

    Tests

---
+++ src/test/java/com/plantwitch/plantwitchbackend/controller/PlantControllerTest.java
@@ -0,0 +1,71 @@
+package com.plantwitch.plantwitchbackend.controller;
+
+import com.plantwitch.plantwitchbackend.entity.Plant;
+import com.plantwitch.plantwitchbackend.service.PlantService;
+import com.plantwitch.plantwitchbackend.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.*;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RestController
+public class PlantControllerTest {
+
+    @Mock
+    private PlantService plantService;
+
+    @InjectMocks
+    private PlantController plantController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(plantController).build();
+    }
+
+    @Test
+    public void createPlant() throws Exception {
+        User user = new User();
+        Plant plant = new Plant();
+        Long userId = 1L;
+        user.setId(userId);
+        plant.setUser(user);
+        plant.setId(1L);
+        plant.setName("Test Plant Name");
+        plant.setRepotDate("2024-07-27");
+        plant.setRepotInterval(12);
+        plant.setWaterDate("2024-07-27");
+        plant.setWaterInterval(6);
+        plant.setImage("image.jpg");
+
+        when(plantService.newPlant(userId, plant)).thenReturn(plant);
+
+        String plantJson = "{\"user_id\":1, \"name\":\"Test Plant Name\",\"waterDate\":\"2024-07-27\",\"repotDate\":\"2024-07-27\",\"waterInterval\":7,\"repotInterval\":12, \"id\":1}";
+
+        mockMvc.perform(post("/api/v1/plants/users/{user_id}", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(plantJson))
+                .andDo(print())  // Log the request and response details
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Test Plant Name"))
+                .andExpect(jsonPath("$.waterDate").value("2024-07-27"))
+                .andExpect(jsonPath("$.repotDate").value("2024-07-27"))
+                .andExpect(jsonPath("$.waterInterval").value(6))
+                .andExpect(jsonPath("$.repotInterval").value(12));
+
+    }
+}
---
+++ src/test/java/com/plantwitch/plantwitchbackend/service/PlantServiceTest.java
@@ -0,0 +1,59 @@
+package com.plantwitch.plantwitchbackend.service;
+
+import com.plantwitch.plantwitchbackend.entity.Plant;
+import com.plantwitch.plantwitchbackend.repository.PlantRepository;
+import com.plantwitch.plantwitchbackend.user.User;
+import com.plantwitch.plantwitchbackend.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+public class PlantServiceTest {
+@Mock
+    private PlantRepository plantRepository;
+
+@Mock
+    private UserRepository userRepository;
+
+@InjectMocks
+    private PlantService plantService;
+
+@BeforeEach
+void setUp() {
+    MockitoAnnotations.openMocks(this);
+}
+
+@Test
+void testCreatePlant() {
+    User user = new User();
+    Plant plant = new Plant();
+    Long userId = 1L;
+    user.setId(userId);
+    plant.setUser(user);
+    plant.setName("Test Plant Name");
+    plant.setRepotDate("2024-07-27");
+    plant.setRepotInterval(12);
+    plant.setWaterDate("2024-07-27");
+    plant.setWaterInterval(6);
+
+    when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+    when(plantRepository.save(plant)).thenReturn(plant);
+
+    Plant createdPlant = plantService.newPlant(userId, plant);
+
+    assertThat(createdPlant).isNotNull();
+    assertThat(createdPlant.getName()).isEqualTo("Test Plant Name");
+    assertThat(createdPlant.getRepotDate()).isEqualTo("2024-07-27");
+    assertThat(createdPlant.getRepotInterval()).isEqualTo(12);
+    assertThat(createdPlant.getWaterDate()).isEqualTo("2024-07-27");
+    assertThat(createdPlant.getWaterInterval()).isEqualTo(6);
+    assertThat(createdPlant.getUser().getId()).isEqualTo(userId);
+}
+}
